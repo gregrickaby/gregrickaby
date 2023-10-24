7fa25cdc8f517b9ae89fdef25271564fc620c62b
Greg Rickaby <gregrickaby@gmail.com>
2023-10-24 13:56:29 -0500

    add config

---
+++ lib/config.ts
@@ -0,0 +1,80 @@
+import {
+  FaAmazon,
+  FaCodepen,
+  FaFacebook,
+  FaGithub,
+  FaGoodreadsG,
+  FaInstagram,
+  FaLinkedin,
+  FaRegFileLines,
+  FaThreads,
+  FaWordpress,
+  FaYoutube
+} from 'react-icons/fa6'
+
+const config = {
+  siteName: 'Greg Rickaby',
+  siteDescription: 'Building websites since 1997.',
+  siteUrl: 'https://gregrickaby.com',
+  siteLogo: '/logo.webp',
+  siteOgImage: '/rickaby-family-fall-2019_1175.webp',
+  socials: [
+    {
+      name: 'CodePen',
+      url: 'https://codepen.io/gregrickaby',
+      icon: FaCodepen
+    },
+    {
+      name: 'GitHub',
+      url: 'https://github.com/gregrickaby/',
+      icon: FaGithub
+    },
+    {
+      name: 'Blog',
+      url: 'https://gregrickaby.com/blog/',
+      icon: FaRegFileLines
+    },
+    {
+      name: 'LinkedIn',
+      url: 'https://linkedin.com/in/gregrickaby/',
+      icon: FaLinkedin
+    },
+    {
+      name: 'WordPress',
+      url: 'https://profiles.wordpress.org/gregrickaby/',
+      icon: FaWordpress
+    },
+    {
+      name: 'Amazon Author',
+      url: 'https://www.amazon.com/author/gregrickaby',
+      icon: FaAmazon
+    },
+    {
+      name: 'Facebook',
+      url: 'https://www.facebook.com/gregrickaby',
+      icon: FaFacebook
+    },
+    {
+      name: 'Goodreads',
+      url: 'https://www.goodreads.com/author/show/16999736.Greg_Rickaby',
+      icon: FaGoodreadsG
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/gregoryrickaby/',
+      icon: FaInstagram
+    },
+    {
+      name: 'Threads',
+      url: 'https://www.threads.net/@gregoryrickaby',
+      icon: FaThreads
+    },
+    {
+      name: 'YouTube',
+      url: 'https://www.youtube.com/@GregRickaby',
+      icon: FaYoutube
+    }
+  ]
+}
+
+export default config
