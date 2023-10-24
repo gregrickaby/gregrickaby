65f1c0fcdc77146a76a8dc43414208ef50c06624
Greg Rickaby <gregrickaby@gmail.com>
2023-10-24 13:57:36 -0500

    add metadata. fire sans. other refactors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,64 @@
+import Footer from '@/components/Footer'
 import Header from '@/components/Header'
-import Meta from '@/components/Meta'
+import Meta from '@/components/JsonLd'
+import config from '@/lib/config'
 import type {Metadata} from 'next'
-import {Inter} from 'next/font/google'
+import {Fira_Sans} from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({subsets: ['latin']})
+/**
+ * Setup Google Font.
+ *
+ * @see https://nextjs.org/docs/app/building-your-application/optimizing/fonts#google-fonts
+ */
+const googleFont = Fira_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['400', '700']
+})
 
+/**
+ * Setup metadata.
+ *
+ * @see https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+ * @see https://nextjs.org/docs/app/api-reference/functions/generate-metadata#basic-fields
+ */
 export const metadata: Metadata = {
-  title: 'Greg Rickaby',
-  description: 'Building websites since 1997'
+  title: config.siteName,
+  description: config.siteDescription,
+  robots: 'follow, index',
+  openGraph: {
+    title: config.siteName,
+    description: config.siteDescription,
+    url: config.siteUrl,
+    siteName: config.siteName,
+    images: [
+      {
+        url: config.siteOgImage,
+        width: 1175,
+        height: 882
+      }
+    ],
+    locale: 'en_US',
+    type: 'website'
+  },
+  themeColor: '#111111',
+  manifest: '/manifest.json'
 }
 
+/**
+ * Root layout component.
+ */
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={`container mx-auto space-y-8 p-12 ${googleFont.className}`}
+      >
         <Meta />
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   )
