12670ea57f11ac46ce070ede5b70817afc025fd0
Greg Rickaby <gregrickaby@gmail.com>
2023-10-24 13:57:57 -0500

    move content here

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
-import Socials from '@/components/Socials'
-
+/**
+ * Home page component.
+ */
 export default function Home() {
   return (
-    <>
-      <main>
-        <Socials />
-      </main>
-    </>
+    <main className="flex flex-col space-y-8 text-center">
+      <p className="text-lg">
+        Disney Dad. Photographer. Published Author. Dev Team Lead at{' '}
+        <a href="https://wpforms.com">WPForms</a>.
+      </p>
+      <a
+        aria-label="Read Greg's blog"
+        className="mx-auto w-56 rounded bg-zinc-900 py-2 text-lg text-white shadow-md transition-colors duration-200 hover:bg-zinc-700"
+        href="https://gregrickaby.com/blog/"
+      >
+        Read my blog
+      </a>
+    </main>
   )
 }
