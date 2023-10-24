ce8a4487ab273012114381544b5dbf1c1e578950
Greg Rickaby <gregrickaby@gmail.com>
2023-10-24 13:58:04 -0500

    add footer with icons

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import SocialLinks from '@/components/SocialLinks'
+
+/**
+ * Footer component.
+ */
+export default function Footer() {
+  return (
+    <footer className="pt-2">
+      <SocialLinks />
+    </footer>
+  )
+}
