22ed1be391dbf2a793a9ebb6ede48aecefd00a44
Greg Rickaby <gregrickaby@gmail.com>
2023-10-24 19:04:30 -0500

    update aria label

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <a href="https://wpforms.com">WPForms</a>.
       </p>
       <a
-        aria-label="Read Greg's blog"
+        aria-label="Read my blog"
         className="mx-auto w-56 rounded bg-zinc-900 py-2 text-lg text-white shadow-md transition-colors duration-200 hover:bg-zinc-700"
         href="https://gregrickaby.com/blog/"
       >
