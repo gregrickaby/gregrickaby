09ae8339f48426539eca40516683b732cab36591
Greg Rickaby <gregrickaby@gmail.com>
2023-11-29 09:01:47 -0600

    rename to <Preload>

--- components/Preload.tsx
+++ components/Preload.tsx
@@ -7,8 +7,7 @@ import ReactDOM from 'react-dom'
  *
  * @see https://nextjs.org/docs/app/api-reference/functions/generate-metadata#resource-hints
  */
-export function PreloadResources() {
-  ReactDOM.preload('/logo.webp', {as: 'image'})
+export function Preload() {
   ReactDOM.preload('/bg_hdr.avif', {as: 'image'})
   return null
 }
