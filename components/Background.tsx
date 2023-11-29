b4c6722ccfc1b6fa7c498981edc96588bdc8fdc6
Greg Rickaby <gregrickaby@gmail.com>
2023-11-29 12:53:55 -0600

    use relative url

--- components/Background.tsx
+++ components/Background.tsx
@@ -4,13 +4,13 @@
 export default function Background() {
   return (
     <picture className="absolute inset-0 h-full w-full">
-      <source srcSet="bg_hdr.avif" type="image/avif" />
-      <source srcSet="bg_hdr.jpg" type="image/jpeg" />
+      <source srcSet="/bg_hdr.avif" type="image/avif" />
+      <source srcSet="/bg_hdr.jpg" type="image/jpeg" />
       <img
         alt=""
         className="h-full w-full object-cover"
         loading="eager"
-        src="bg_hdr.jpg"
+        src="/bg_hdr.jpg"
       />
     </picture>
   )
