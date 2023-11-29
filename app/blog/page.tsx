ee3ec96e3e9337a5c7a991f7cddda8f3c304d0b5
Greg Rickaby <gregrickaby@gmail.com>
2023-11-29 09:10:42 -0600

    add under construction

---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+/**
+ * Blog component.
+ */
+export default async function Blog() {
+  return (
+    <main className="flex flex-col space-y-8 text-center">
+      <p className="text-lg">Under Construction</p>
+    </main>
+  )
+}
