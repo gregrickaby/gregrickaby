10ab1739c338f323de23b91567aa1a7c4e184e25
Greg Rickaby <gregrickaby@gmail.com>
2023-11-29 10:33:17 -0600

    link back to homepage

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import config from '@/lib/config'
 import Image from 'next/image'
+import Link from 'next/link'
 
 /**
  * Header component.
@@ -7,14 +8,16 @@ import Image from 'next/image'
 export default async function Header() {
   return (
     <header className="flex flex-col justify-center gap-8 text-center">
-      <Image
-        alt="Greg Rickaby"
-        className="mx-auto h-24 w-24 rounded-full shadow-lg"
-        height={96}
-        loading="eager"
-        src={config.siteLogo}
-        width={96}
-      />
+      <Link href="/">
+        <Image
+          alt="Greg Rickaby"
+          className="mx-auto h-24 w-24 rounded-full shadow-lg"
+          height={96}
+          loading="eager"
+          src={config.siteLogo}
+          width={96}
+        />
+      </Link>
       <h1 className="text-4xl font-bold leading-none">{config.siteName}</h1>
     </header>
   )
