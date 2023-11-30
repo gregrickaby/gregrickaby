fc017cd9dad920a52bd6e47588d95aaf801d28d4
Greg Rickaby <gregrickaby@gmail.com>
2023-11-30 06:55:08 -0600

    adjust order of content

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,15 +8,6 @@ export default function NotFound() {
   return (
     <main className="flex flex-col space-y-8 text-center">
       <h1 className="text-3xl font-bold">404 - Not Found</h1>
-      <p>
-        The missing page might either be on{' '}
-        <a href="https://gregrickaby.com/blog/">my blog</a> or on the{' '}
-        <a href="https://web.archive.org/web/20220101000000*/https://gregrickaby.com">
-          Wayback Machine
-        </a>
-        .
-      </p>
-      <Link href="/">Return Home</Link>
       <Image
         alt=""
         className="mx-auto"
@@ -25,6 +16,14 @@ export default function NotFound() {
         src="/404.webp"
         width="160"
       />
+      <p>
+        The missing page might be on the{' '}
+        <a href="https://web.archive.org/web/20220101000000*/https://gregrickaby.com">
+          Wayback Machine
+        </a>
+        .
+      </p>
+      <Link href="/">Return Home</Link>
     </main>
   )
 }
