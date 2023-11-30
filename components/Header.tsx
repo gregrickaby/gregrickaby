34bd53dfce88ad157696eb1bfe499931aa9716ce
Greg Rickaby <gregrickaby@gmail.com>
2023-11-30 06:56:22 -0600

    responseive markup. add nav.

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,18 +7,33 @@ import Link from 'next/link'
  */
 export default async function Header() {
   return (
-    <header className="flex flex-col justify-center gap-8 text-center">
-      <Link href="/">
-        <Image
-          alt="Greg Rickaby"
-          className="mx-auto h-24 w-24 rounded-full shadow-lg"
-          height={96}
-          loading="eager"
-          src={config.siteLogo}
-          width={96}
-        />
-      </Link>
-      <h1 className="text-4xl font-bold leading-none">{config.siteName}</h1>
+    <header className="flex flex-col items-center justify-center gap-12 pb-12 text-center md:flex-row md:justify-between">
+      <div className="flex flex-col items-center gap-4 md:flex-row md:text-left">
+        <Link href="/">
+          <Image
+            alt="Greg Rickaby"
+            className="mx-auto h-24 w-24 rounded-full shadow-lg"
+            height={96}
+            loading="eager"
+            src={config.siteLogo}
+            width={96}
+          />
+        </Link>
+        <div className="flex flex-col md:text-left">
+          <Link href="/">
+            <h1 className="mb-3 text-4xl font-bold leading-none">
+              {config.siteName}
+            </h1>
+          </Link>
+          <p className="m-0 max-w-sm p-0 text-lg">{config.intro}</p>
+        </div>
+      </div>
+      <nav className="flex justify-center gap-8 text-center text-xl">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/blog">Blog</Link>
+        <Link href="/contact">Contact</Link>
+      </nav>
     </header>
   )
 }
