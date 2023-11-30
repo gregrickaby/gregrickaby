5eb2d38aff56a1964220f6a7012a256b13756b1c
Greg Rickaby <gregrickaby@gmail.com>
2023-11-30 06:57:23 -0600

    blog route shows blog archive

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,5 @@
-import config from '@/lib/config'
-import getAllPosts from '@/lib/queries/getAllPosts'
+import getPosts from '@/lib/queries/getPosts'
 import {Post} from '@/lib/types'
-import type {Metadata} from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
 import {notFound} from 'next/navigation'
@@ -15,24 +13,13 @@ export const dynamic = 'force-dynamic'
 export const revalidate = 3600
 
 /**
- * Setup metadata.
- *
- * @see https://nextjs.org/docs/app/building-your-application/optimizing/metadata
- * @see https://nextjs.org/docs/app/api-reference/functions/generate-metadata#basic-fields
- */
-export const metadata: Metadata = {
-  title: `${config.siteName} - Blog Archive`,
-  description: 'The latest posts from the blog.'
-}
-
-/**
- * The blog archive route.
+ * The blog archive.
  *
  * @see https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#pages
  */
-export default async function Archive() {
+export default async function Blog() {
   // Fetch posts from WordPress.
-  const posts = await getAllPosts()
+  const posts = await getPosts(15)
 
   // No data? Bail...
   if (!posts || !posts.length) {
@@ -40,37 +27,35 @@ export default async function Archive() {
   }
 
   return (
-    <main className="container m-auto flex flex-col gap-8">
-      <aside>
-        <h2>Latest Posts</h2>
-        <div className="flex flex-wrap gap-8">
-          {posts.map((post: Post) => (
-            <article className="w-72" key={post.databaseId}>
-              <Image
-                alt={post?.featuredImage?.node?.altText}
-                height={
-                  post?.featuredImage?.node?.mediaDetails?.sizes[0]?.height
-                }
-                src={
-                  post?.featuredImage?.node?.mediaDetails?.sizes[0]?.sourceUrl
-                }
-                width={post?.featuredImage?.node?.mediaDetails?.sizes[0]?.width}
-                priority={true}
-              />
+    <main className="flex flex-col gap-8">
+      <h2 className="m-0 text-3xl">Latest Posts</h2>
+      <div className="flex flex-wrap gap-8">
+        {posts.map((post: Post) => (
+          <article className="max-w-[309px]" key={post.databaseId}>
+            {post.featuredImage?.node?.mediaDetails?.sizes?.[0] && (
               <Link href={`/blog/${post.slug}`}>
-                <h2 dangerouslySetInnerHTML={{__html: post.title}} />
+                <Image
+                  alt={post.featuredImage.node.altText}
+                  height={post.featuredImage.node.mediaDetails.sizes[0].height}
+                  src={post.featuredImage.node.mediaDetails.sizes[0].sourceUrl}
+                  width={post.featuredImage.node.mediaDetails.sizes[0].width}
+                  priority={true}
+                />
               </Link>
-              <p className="text-sm text-gray-500">
-                {post.commentCount} Comments
-              </p>
-              <div dangerouslySetInnerHTML={{__html: post.excerpt}} />
-              <Link className="button" href={`/blog/${post.slug}`}>
-                View Post
-              </Link>
-            </article>
-          ))}
-        </div>
-      </aside>
+            )}
+            <Link href={`/blog/${post.slug}`}>
+              <h2 dangerouslySetInnerHTML={{__html: post.title}} />
+            </Link>
+            <p className="text-sm text-gray-500">
+              {post.commentCount > 0 ? post.commentCount : 0} comments
+            </p>
+            <div dangerouslySetInnerHTML={{__html: post.excerpt}} />
+            <Link className="button" href={`/blog/${post.slug}`}>
+              View Post
+            </Link>
+          </article>
+        ))}
+      </div>
     </main>
   )
 }
