1a051694dc8e1e87b343a10769e159a2ec2822a6
Greg Rickaby <gregrickaby@gmail.com>
2023-11-30 06:56:40 -0600

    add contact route

---
+++ app/contact/page.tsx
@@ -0,0 +1,91 @@
+export default function Contact() {
+  return (
+    <main className="mx-auto flex max-w-3xl flex-col gap-8">
+      <article>
+        <h1>Contact</h1>
+        <p>Feel free to send an email to greg@gregrickaby.com</p>
+
+        <p>You can also find me on these platforms:</p>
+        <ul>
+          <li>
+            <a
+              href="https://www.instagram.com/gregoryrickaby/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Instagram
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://www.facebook.com/gregrickaby"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Facebook
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://github.com/gregrickaby"
+              target="_blank"
+              rel="noopener"
+              title=""
+            >
+              Github
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://www.goodreads.com/gregrickaby"
+              target="_blank"
+              rel="noopener"
+              title=""
+            >
+              Goodreads
+            </a>
+          </li>
+
+          <li>
+            <a href="http://www.linkedin.com/in/gregrickaby" title="">
+              LinkedI
+            </a>
+            <a
+              href="http://www.linkedin.com/in/gregrickaby"
+              target="_blank"
+              rel="noopener"
+              title=""
+            >
+              n
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://www.threads.net/@gregoryrickaby"
+              target="_blank"
+              rel="noopener"
+              title=""
+            >
+              Threads
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://www.youtube.com/@GregRickaby/"
+              target="_blank"
+              rel="noopener"
+              title=""
+            >
+              YouTube
+            </a>
+          </li>
+        </ul>
+      </article>
+    </main>
+  )
+}
