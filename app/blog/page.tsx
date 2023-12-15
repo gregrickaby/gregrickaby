2d65057160d31b2db20f441956eef6a0c82a8a09
Greg Rickaby <gregrickaby@gmail.com>
2023-12-15 15:33:08 -0600

    limit to 15

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,20 +3,13 @@ import getPosts from '@/lib/queries/getPosts'
 import {notFound} from 'next/navigation'
 
 /**
- * Route segment config.
- *
- * @see https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
- */
-export const revalidate = 3600
-
-/**
  * The blog archive.
  *
  * @see https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#pages
  */
 export default async function Blog() {
   // Fetch posts from WordPress.
-  const posts = await getPosts(150)
+  const posts = await getPosts(15)
 
   // No posts? Throw a 404.
   if (!posts) {
