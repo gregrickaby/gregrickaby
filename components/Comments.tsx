db9be01dac85d101fe867f0a6453a8d1c1f70890
Greg Rickaby <gregrickaby@gmail.com>
2023-12-28 10:57:00 -0600

    update destructuring

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -13,28 +13,30 @@ export default function Comments({post}: CommentsProps) {
     <>
       <section className="border-t-2 border-zinc-500 py-4">
         <h3 className="text-3xl font-bold">Comments</h3>
-        {!post.comments.nodes.length && (
+        {!!post.comments.edges.length && (
           <p className="italic">No comments yet.</p>
         )}
-        {post.comments.nodes.map((comment) => (
-          <article key={comment.databaseId}>
+        {post.comments.edges.map((comment) => (
+          <article key={comment.node.databaseId}>
             <header className="flex items-center gap-2">
               <img
-                alt={comment.author.node.name}
+                alt={comment.node.author.node.name}
                 className="m-0 rounded-full"
                 height={64}
-                src={comment.author.node.gravatarUrl}
+                src={comment.node.author.node.avatar.url}
                 width={64}
               />
               <div className="flex flex-col gap-2">
                 <h4
                   className="m-0 p-0 leading-none"
-                  dangerouslySetInnerHTML={{__html: comment.author.node.name}}
+                  dangerouslySetInnerHTML={{
+                    __html: comment.node.author.node.name
+                  }}
                 />
-                <time className="italic">{comment.date}</time>
+                <time className="italic">{comment.node.date}</time>
               </div>
             </header>
-            <div dangerouslySetInnerHTML={{__html: comment.content}} />
+            <div dangerouslySetInnerHTML={{__html: comment.node.content}} />
           </article>
         ))}
       </section>
