14b6bca3a54fbb7d6e5459a5f6aa37ea725dd7e0
Greg Rickaby <gregrickaby@gmail.com>
2023-12-28 11:08:05 -0600

    check for url first

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -13,19 +13,21 @@ export default function Comments({post}: CommentsProps) {
     <>
       <section className="border-t-2 border-zinc-500 py-4">
         <h3 className="text-3xl font-bold">Comments</h3>
-        {!!post.comments.edges.length && (
+        {!!post?.comments?.edges?.length && (
           <p className="italic">No comments yet.</p>
         )}
         {post.comments.edges.map((comment) => (
           <article key={comment.node.databaseId}>
             <header className="flex items-center gap-2">
-              <img
-                alt={comment.node.author.node.name}
-                className="m-0 rounded-full"
-                height={64}
-                src={comment.node.author.node.avatar.url}
-                width={64}
-              />
+              {!!comment?.node?.author?.node?.avatar?.url && (
+                <img
+                  alt={comment.node.author.node.name}
+                  className="m-0 rounded-full"
+                  height={64}
+                  src={comment.node.author.node.avatar.url}
+                  width={64}
+                />
+              )}
               <div className="flex flex-col gap-2">
                 <h4
                   className="m-0 p-0 leading-none"
