92ba359c3c5980e64b56f8f80879d0e859c83c39
Greg Rickaby <gregrickaby@gmail.com>
2024-07-01 07:10:55 -0500

    working through transition back to geocities

--- geocities/script.js
+++ geocities/script.js
@@ -1,59 +1,97 @@
-// Set global state.
+/**
+ * Add event listener to the toggle button.
+ */
+document.body.addEventListener("click", function (event) {
+  if (event.target.id === "toggleGeocities") {
+    toggleGeocities();
+  }
+});
+
+// Set the initial state.
 let isGeocities = false;
+let originalHeader, originalMain, originalFooter;
 
 /**
- * Geocities theme!
+ * Toggle Geocities theme.
  */
-function geocities() {
-  // Toggle the state.
+function toggleGeocities() {
+  // Get the loading screen and audio.
+  const loadingScreen = document.getElementById("loading");
+  const loadingAudio = document.getElementById("loadingAudio");
+
   if (isGeocities) {
+    document
+      .querySelector('head link[href="geocities/geocities.css"]')
+      .remove();
+    document.querySelector("header").innerHTML = originalHeader;
+    document.querySelector("main").innerHTML = originalMain;
+    document.querySelector("footer").innerHTML = originalFooter;
+    loadingScreen.style.display = "none";
+    loadingAudio.pause();
     isGeocities = false;
-    return window.location.reload(true);
+  } else {
+    // Show loading screen.
+    loadingScreen.style.display = "flex";
+
+    // Play loading audio.
+    loadingAudio.play();
+
+    // Save the original state.
+    originalHeader = document.querySelector("header").innerHTML;
+    originalMain = document.querySelector("main").innerHTML;
+    originalFooter = document.querySelector("footer").innerHTML;
+
+    // Add Geocities styles.
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "geocities/geocities.css";
+    document.head.appendChild(link);
+
+    setTimeout(() => {
+      // Header HTML content.
+      const newHeader = `
+            <img src="geocities/welcome.webp" alt="Welcome to my homepage!" loading="eager" />
+            <div>
+              <marquee width="350px" behavior="scroll" direction="right" scrollamount="12">
+                <img src="geocities/enterprise.webp" alt="Enterprise" loading="eager" />
+              </marquee>
+              <div class="boldy-going">
+              <img src="geocities/earth.webp" alt="Earth" loading="eager" />
+              <marquee width="350px" behavior="scroll">
+                <span role="image" aria-label="spock">🖖🏻</span> To boldly go where no one has gone before <span role="image" aria-label="spock">🖖🏻</span>
+              </marquee>
+              <img src="geocities/earth.webp" alt="Earth" loading="eager" />
+              </div>
+            </div>
+          `;
+
+      // Main content HTML.
+      const flamingHoffTrek = `
+            <img src="geocities/bridge-crew.webp" alt="Bridge crew" loading="lazy" />
+            <img src="geocities/hasselhoff.webp" alt="David Hasselhoff" loading="lazy" />
+            <img src="geocities/flames.webp" alt="Flames" loading="lazy" />
+          `;
+
+      // Footer HTML content.
+      const newFooter = `
+            <p>
+              <img src="geocities/counter.webp" alt="Counter" loading="lazy" />
+              <img src="geocities/netscape.webp" alt="Netscape" loading="lazy" />
+              <img src="geocities/ie.webp" alt="Internet Explorer" loading="lazy" />
+              <img src="geocities/notepad.webp" alt="Notepad" loading="lazy" />
+            </p>
+          `;
+
+      // Update the DOM.
+      document.querySelector("header").innerHTML = newHeader;
+      document.querySelector("main").innerHTML += flamingHoffTrek;
+      document.querySelector("footer").innerHTML += newFooter;
+
+      // Hide loading screen.
+      loadingScreen.style.display = "none";
+
+      // Update state.
+      isGeocities = true;
+    }, 5700); // 5700ms is the duration of the dial-up sound.
   }
-
-  const assets = `
-    <link rel="stylesheet" href="geocities/styles.css" />
-  `;
-
-  const newHeader = `
-  <img src="geocities/welcome.gif" alt="Welcome to my homepage!" loading="eager" />
-  <div>
-
-    <marquee width="350px" behavior="scroll" direction="right" scrollamount="12">
-      <img src="geocities/enterprise.webp" alt="Enterprise" loading="eager" />
-    </marquee>
-    <div></div>
-    <img src="geocities/earth.gif" alt="" loading="eager" />
-    <marquee width="350px" behavior="scroll">
-      <span role="image" aria-label="spock">🖖🏻</span> To boldy go where no one has gone before <span role="image" aria-label="spock">🖖🏻</span>
-    </marquee>
-    <img src="geocities/earth.gif" alt="" loading="eager" />
-  </div>
-  `;
-
-  // David, you beatiful SOB.
-  const flamingHoffTrek = `
-  <img src="geocities/bridge-crew.webp" alt="" loading="lazy" />
-  <img src="geocities/hasselhoff.webp" alt="" loading="lazy" />
-  <img src="geocities/flames.gif" alt="" loading="lazy" />
-  `;
-
-  const newFooter = `
-  <p>
-    <img src="geocities/counter.gif" alt="" loading="lazy" />
-    <img src="geocities/netscape.gif" alt="" loading="lazy" />
-    <img src="geocities/ie.gif" alt="" loading="lazy" />
-    <img src="geocities/notepad.gif" alt="" loading="lazy" />
-    <audio src="geocities/star-trek-modem.mp3" autoplay loop />
-  </p>
-  `;
-
-  // Add or append HTML.
-  document.querySelector('head').innerHTML += assets;
-  document.querySelector('header').innerHTML = newHeader;
-  document.querySelector('main').innerHTML += flamingHoffTrek;
-  document.querySelector('footer').innerHTML += newFooter;
-
-  // Update state.
-  isGeocities = true;
 }
