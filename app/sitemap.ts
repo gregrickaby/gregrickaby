02c9f4032d2e0963087503b9e6cf24fad8b9e4d1
Greg Rickaby <gregrickaby@gmail.com>
2024-07-18 10:55:12 -0500

    add sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,32 @@
+import {getAllPages, getAllPosts} from '@/lib/api'
 import config from '@/lib/config'
+import {Post} from '@/lib/types'
 import {MetadataRoute} from 'next'
 
 /**
- * Route segment config.
- *
- * @see https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config
+ * Helper function to generate sitemap entries.
  */
-export const revalidate = 43200 // 12 hours
+function generateSitemapEntries(
+  items: Post[],
+  urlPrefix: string
+): MetadataRoute.Sitemap {
+  return (
+    items
+
+      // Filter out any items that are set to noindex.
+      .filter((item) => item.yoast_head_json.robots.index !== 'noindex')
+
+      // Map the items to sitemap entries.
+      .map((item) => ({
+        url: `${config.siteUrl}${urlPrefix}${item.slug}`,
+        lastModified: new Date(
+          item.yoast_head_json.article_modified_time
+        ).toISOString(),
+        changeFrequency: 'monthly',
+        priority: 0.5
+      }))
+  )
+}
 
 /**
  * The sitemap.xml route.
@@ -14,12 +34,24 @@ export const revalidate = 43200 // 12 hours
  * @see https://nextjs.org/docs/app/api-reference/file-conventions/metadata/sitemap
  */
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  return [
-    {
-      url: config.siteUrl,
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1
-    }
-  ]
+  // Fetch all the pages and posts.
+  const allPages = await getAllPages()
+  const allPosts = await getAllPosts()
+
+  // Generate sitemap entries for pages.
+  const pagesSitemapEntries = generateSitemapEntries(
+    allPages.filter((page) => page.slug !== 'home'), // Exclude the home page.
+    '/'
+  )
+
+  console.log(pagesSitemapEntries)
+
+  // Generate sitemap entries for posts.
+  const postsSitemapEntries = generateSitemapEntries(allPosts, '/blog/')
+
+  // Combine entries.
+  const sitemapEntries = [...pagesSitemapEntries, ...postsSitemapEntries]
+
+  // Return the sitemap.
+  return sitemapEntries
 }
--- lib/api.ts
+++ lib/api.ts
@@ -1,18 +1,18 @@
 import {Post} from '@/lib/types'
 
 /**
- * Get all posts from the WordPress REST API with pagination.
+ * Generic function to query data from the WordPress REST API.
  */
-export async function getAllPosts(): Promise<Post[]> {
+async function query(endpoint: string, query: string): Promise<Post[]> {
   try {
     const response = await fetch(
-      'https://blog.gregrickaby.com/wp-json/wp/v2/posts?per_page=100&_fields=id,slug,title',
+      `https://blog.gregrickaby.com/wp-json/wp/v2/${endpoint}?${query}`,
       {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json'
         },
-        next: {revalidate: 3600}
+        next: {revalidate: 43200} // 12 hours
       }
     )
 
@@ -23,7 +23,7 @@ export async function getAllPosts(): Promise<Post[]> {
     const data: Post[] = await response.json()
 
     if (data.length === 0) {
-      throw new Error('No posts found')
+      throw new Error('No data found')
     }
 
     return data
@@ -34,37 +34,45 @@ export async function getAllPosts(): Promise<Post[]> {
 }
 
 /**
- * Get a post from the WordPress REST API by slug.
+ * Get all pages.
+ */
+export async function getAllPages(): Promise<Post[]> {
+  return await query(
+    'pages',
+    'per_page=100&_fields=id,slug,title,yoast_head_json'
+  )
+}
+
+/**
+ * Get all posts.
+ */
+export async function getAllPosts(): Promise<Post[]> {
+  return await query(
+    'posts',
+    'per_page=100&_fields=id,slug,title,yoast_head_json'
+  )
+}
+
+/**
+ * Get a page by slug.
+ */
+export async function getPageBySlug(slug: string): Promise<Post> {
+  const data = await query(
+    'pages',
+    `slug=${slug}&_fields=id,date,slug,status,title,excerpt,content,acf,category_names,tag_names,featured_image_data,yoast_head_json`
+  )
+  return data[0]
+}
+
+/**
+ * Get a post by slug.
  */
 export async function getPostBySlug(slug: string): Promise<Post> {
-  // Remove "/blog/" from the slug.
+  // Remove "/blog/" from the incoming slug.
   slug = slug.replace(/^\/blog\//, '')
-
-  try {
-    const response = await fetch(
-      `https://blog.gregrickaby.com/wp-json/wp/v2/posts?slug=${slug}&_fields=id,date,slug,status,title,excerpt,content,acf,category_names,tag_names,featured_image_data,yoast_head_json`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        next: {revalidate: 3600}
-      }
-    )
-
-    if (!response.ok) {
-      throw new Error('Network response was not ok')
-    }
-
-    const data: Post[] = await response.json()
-
-    if (data.length === 0) {
-      throw new Error('Post not found')
-    }
-
-    return data[0]
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
+  const data = await query(
+    'posts',
+    `slug=${slug}&_fields=id,date,slug,status,title,excerpt,content,acf,category_names,tag_names,featured_image_data,yoast_head_json`
+  )
+  return data[0]
 }
