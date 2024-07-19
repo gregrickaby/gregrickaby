d7558b765a0b9dcc58c3e02f97f6d4a9bc291d79
Greg Rickaby <greg.rickaby@mindsize.com>
2024-07-19 11:12:11 -0500

    don't show image

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,4 @@
 import {getPosts} from '@/lib/api'
-import Image from 'next/image'
 import Link from 'next/link'
 import {notFound} from 'next/navigation'
 
@@ -18,19 +17,10 @@ export default async function BlogArchive() {
   return (
     <>
       <h1>Blog</h1>
-      <div className="not-prose grid grid-cols-3 gap-4">
+      <div className="not-prose flex flex-col gap-4">
         {posts.map((post) => (
           <article key={post.id}>
-            <Link className="flex flex-col gap-4" href={`/blog/${post.slug}`}>
-              {post.featured_image_data.url && (
-                <Image
-                  alt={post.featured_image_data.alt || ''}
-                  className=""
-                  height={post.featured_image_data.height}
-                  src={post.featured_image_data.url}
-                  width={post.featured_image_data.width}
-                />
-              )}
+            <Link className="" href={`/blog/${post.slug}`}>
               <h2
                 className="text-xl hover:underline"
                 dangerouslySetInnerHTML={{__html: post.title.rendered}}
