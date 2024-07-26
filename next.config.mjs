c77c442879e6533c9c5282027e88794d1cf890ed
Greg Rickaby <greg.rickaby@mindsize.com>
2024-07-26 11:56:20 -0500

    stop using next canary
    'use client' stopped working

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
-import type {NextConfig} from 'next'
-
 /**
  * Next.js configuration.
  *
  * @see https://nextjs.org/docs/app/api-reference/next-config-js
  */
-const nextConfig: NextConfig = {
+const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
