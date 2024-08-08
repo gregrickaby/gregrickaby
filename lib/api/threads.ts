c8031e801acbe48bf992b265ab375f9c74da5632
Greg Rickaby <gregrickaby@gmail.com>
2024-08-08 10:53:41 -0500

    filter out text-based threads

--- lib/api/threads.ts
+++ lib/api/threads.ts
@@ -40,22 +40,22 @@ export interface Cursors {
  * Get the latest posts from Threads.
  */
 export async function getThreads(posts: number): Promise<Threads> {
-  // Create a new URL object.
+  // Create a new URL object for the Threads API endpoint.
   const url = new URL('https://graph.threads.net/v1.0/me/threads')
 
-  // Add the field query parameters.
+  // Add the field query parameters to specify which fields to include in the response.
   url.searchParams.append(
     'fields',
     'id,is_quote_post,media_product_type,media_type,media_url,permalink,text,timestamp,username'
   )
 
-  // Add the limit query parameter.
+  // Add the limit query parameter to specify the number of posts to retrieve.
   url.searchParams.append('limit', posts.toString())
 
   // Get the access token from the environment variables.
   const accessToken = process.env.THREADS_ACCESS_TOKEN
 
-  // No access token, throw an error.
+  // If the access token is not set, throw an error.
   if (!accessToken) {
     throw new Error('Access token is not set in environment variables!')
   }
@@ -64,6 +64,7 @@ export async function getThreads(posts: number): Promise<Threads> {
   url.searchParams.append('access_token', accessToken)
 
   try {
+    // Send the request to the Threads API.
     const response = await fetch(url.toString())
 
     // If the response is not ok, throw an error.
@@ -75,14 +76,20 @@ export async function getThreads(posts: number): Promise<Threads> {
     const threads: Threads = await response.json()
 
     // If there is no data or the data is empty, throw an error.
-    if (!threads || threads.data.length === 0) {
+    if (!threads.data || threads.data.length === 0) {
       throw new Error('No data returned')
     }
 
-    // Return the data.
+    // Filter out threads that are quote posts.
+    threads.data = threads.data.filter((thread) => !thread.is_quote_post)
+
+    // Filter out threads that don't have a media URL.
+    threads.data = threads.data.filter((thread) => thread.media_url)
+
+    // Return the filtered data.
     return threads
   } catch (error) {
-    console.error('Error fetching threads', error)
-    throw error
+    console.error('Unexpected error fetching threads:', error)
+    throw new Error('Unexpected error fetching threads')
   }
 }
