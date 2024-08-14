278bbd55c38e600b4221b6e8bf0a892d0c333596
Greg Rickaby <gregrickaby@gmail.com>
2024-08-14 01:06:31 -0500

    simplify homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import {ArticleCard} from '@/components/ArticleCard'
-import {WP_Query, getGithubRepos} from '@/lib/api'
+import {WP_Query} from '@/lib/api'
 import Link from 'next/link'
 
 const latestPosts = new WP_Query({
@@ -17,103 +17,39 @@ const latestPosts = new WP_Query({
  */
 export default async function Home() {
   const posts = await latestPosts.getPosts()
-  const repos = await getGithubRepos(5)
 
   return (
-    <div className="two-col">
-      <article className="article">
-        <header>
-          <h1>Hello There 👋</h1>
-          <p>
-            I&apos;m a{' '}
-            <a
-              aria-label="follow on LinkedIn"
-              href="https://www.linkedin.com/in/gregrickaby/"
-              rel="author"
-            >
-              full-stack engineer
-            </a>
-            , <Link href="/photos">photography enthusiast</Link>, and{' '}
-            <a
-              aria-label="view my amazon author profile"
-              href="https://www.amazon.com/author/gregrickaby"
-              rel="author"
-            >
-              published author
-            </a>{' '}
-            living in Southeast Alabama. I&apos;m married with three kids and
-            some of my other hobbies include cooking, traveling, and reading.
-          </p>
-        </header>
+    <div className="article">
+      <h1>Hello There 👋</h1>
+      <p>
+        I&apos;m a{' '}
+        <a
+          aria-label="follow on LinkedIn"
+          href="https://www.linkedin.com/in/gregrickaby/"
+          rel="author"
+        >
+          full-stack engineer
+        </a>
+        , <Link href="/photos">photography enthusiast</Link>, and{' '}
+        <a
+          aria-label="view my amazon author profile"
+          href="https://www.amazon.com/author/gregrickaby"
+          rel="author"
+        >
+          published author
+        </a>{' '}
+        living in Southeast Alabama. I&apos;m married with three kids and some
+        of my other hobbies include cooking, traveling, and reading.
+      </p>
 
-        <h2>Latest Posts</h2>
-        {posts.map((post) => (
-          <ArticleCard key={post.id} post={post} />
-        ))}
+      <h2>Latest Posts</h2>
+      {posts.map((post) => (
+        <ArticleCard key={post.id} post={post} />
+      ))}
 
-        <footer className="mt-8 text-center">
-          <Link href="/blog">See all blog posts</Link>
-        </footer>
-      </article>
-
-      <aside className="sidebar">
-        <div>
-          <h3>My Hobby Apps</h3>
-          <ul>
-            <li>
-              <a href="https://reddit-viewer.com">Reddit Viewer</a>
-            </li>
-            <li>
-              <a href="https://localwx.vercel.app">Local Weather</a>
-            </li>
-            <li>
-              <a href="https://spellingscramble.vercel.app">
-                Spelling Scramble
-              </a>
-            </li>
-            <li>
-              <a href="https://preschool-flashcards.vercel.app">
-                Preschool Flashcards
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div>
-          <h3>Popular Repos</h3>
-          <ul>
-            {repos.map((repo) => (
-              <li key={repo.id}>
-                <a href={repo.html_url}>{repo.name}</a> ({repo.stargazers_count}
-                ⭐️)
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div>
-          <h3>Blogroll</h3>
-          <ul>
-            <li>
-              <a href="https://petapixel.com/">PetaPixel</a>
-            </li>
-            <li>
-              <a href="https://www.dpreview.com/">DP Review</a>
-            </li>
-            <li>
-              <a href="https://news.ycombinator.com/">Hacker News</a>
-            </li>
-            <li>
-              <a href="https://www.timeanddate.com/news/astronomy/">
-                Astronomy News
-              </a>{' '}
-            </li>
-            <li>
-              <a href="https://www.disneytouristblog.com/">
-                Disney Tourist Blog
-              </a>{' '}
-            </li>
-          </ul>
-        </div>
-      </aside>
+      <footer className="mt-8 text-center">
+        <Link href="/blog">See all blog posts</Link>
+      </footer>
     </div>
   )
 }
