641290e1862f860c02d4c255fc424c04623223de
Greg Rickaby <gregrickaby@gmail.com>
2025-01-10 19:36:08 -0600

    add metadata

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,24 @@
 import {ContactForm} from '@/components/ContactForm/ContactForm'
 
+import config from '@/lib/config'
+import {Metadata} from 'next'
+
+/**
+ * Generate metadata.
+ *
+ * @see https://nextjs.org/docs/app/api-reference/functions/generate-metadata#generatemetadata-function
+ */
+export function generateMetadata(): Metadata {
+  return {
+    title: `Contact - ${config.siteName}`,
+    description:
+      'Get in touch with me by filling out the form below. I will get back to you as soon as possible.',
+    alternates: {
+      canonical: '/contact'
+    }
+  }
+}
+
 /**
  * Contact page.
  */
