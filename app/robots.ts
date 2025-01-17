8d6b2bc7a728cb9cb67782f50f3e559ffc869bea
Greg Rickaby <gregrickaby@gmail.com>
2025-01-16 18:41:33 -0600

    block ai

--- app/robots.ts
+++ app/robots.ts
@@ -1,17 +1,40 @@
 import config from '@/lib/config'
-import {MetadataRoute} from 'next'
 
 /**
  * The robots.txt route.
  *
  * @see https://nextjs.org/docs/app/api-reference/file-conventions/metadata/robots
  */
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
+export default function robots() {
+  // List of user agents to block.
+  const disallowedUserAgents = [
+    'CCBot',
+    'ChatGPT-User',
+    'Claude-Web',
+    'GPTBot',
+    'Google-Extended',
+    'PerplexityBot',
+    'anthropic-ai'
+  ]
+
+  // Create disallow rules for specific user agents.
+  const disallowRules = disallowedUserAgents.map((userAgent) => ({
+    userAgent,
+    disallow: '/'
+  }))
+
+  // Rules for the robots.txt.
+  const rules = [
+    {
       userAgent: '*',
       allow: '/'
     },
+    ...disallowRules
+  ]
+
+  // Return the rules and the sitemap URL.
+  return {
+    rules,
     sitemap: `${config.siteUrl}/sitemap.xml`
   }
 }
