aba99636d525ddaebbdedb684ec48655c6fdbfa5
Greg Rickaby <gregrickaby@gmail.com>
2025-04-17 10:22:42 -0500

    get bio from wordpress

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -3,7 +3,7 @@ import styles from './HomePage.module.css'
 /**
  * The homepage component.
  */
-export async function HomePage() {
+export async function HomePage({bio}: Readonly<{bio: string}>) {
   return (
     <article className={styles.homePage}>
       <h1 className={styles.title}>
@@ -12,29 +12,7 @@ export async function HomePage() {
         </span>{' '}
         Hello there!
       </h1>
-      <p className={styles.intro}>
-        I&apos;m a Tech Lead at{' '}
-        <a
-          href="https://mindsize.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Mindsize
-        </a>
-        ,{' '}
-        <a href="https://www.amazon.com/author/gregrickaby" rel="author">
-          published author
-        </a>
-        , and proud survivor of the{' '}
-        <a href="https://gregrickaby.com/geocities">Geocities era</a>.{' '}
-        <span className="hidden md:inline">
-          Outside of work, you&apos;ll find me{' '}
-          <a href="https://flickr.com/people/gregrickaby/" rel="author">
-            behind a camera
-          </a>
-          , tossing pizzas, or planning the next family road trip.
-        </span>
-      </p>
+      <section className={styles.bio} dangerouslySetInnerHTML={{__html: bio}} />
     </article>
   )
 }
