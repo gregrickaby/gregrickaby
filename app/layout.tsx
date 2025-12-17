a18a1460ae0a4e73aeda6a33ec2dd475d590bc92
Greg Rickaby <gregrickaby@gmail.com>
2025-12-17 16:16:43 -0600

    update default theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="luxury" suppressHydrationWarning>
+    <html lang="en" data-theme="light" suppressHydrationWarning>
       <head>
         <script
           dangerouslySetInnerHTML={{
